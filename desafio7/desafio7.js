594947d4c6ad4a1f37f585be2781ad4bf207cdf3
Sabrina <sabrinademarcoet36@gmail.com>
2024-05-31 20:09:54 -0300

    (mensaje del commit)

---
+++ desafio7/desafio7.js
@@ -0,0 +1,5 @@
+let precioP = document.getElementById('precioProducto')
+precioP.innerText = 'precio: $450';  //Para cambiar el texto el inner text//
+let titulo=document.querySelector('h1')
+titulo.style.color='cyan'
+titulo.classList.add('color', 'fondo')
